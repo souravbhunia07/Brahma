3d86c3a1dfa149de6897332360e97ee0b1f82b60
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2024-11-29 21:19:35 +0530

    succesfully implemented parser for the interpreter

--- src/Brahma.java
+++ src/Brahma.java
@@ -45,9 +45,17 @@ public class Brahma {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        for(Token token : tokens) {
-            System.out.println(token);
-        }
+
+//        for(Token token : tokens) {
+//            System.out.println(token);
+//        }
+
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -57,5 +65,14 @@ public class Brahma {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        }
+        else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,11 +1,21 @@
 import java.util.List;
 
 public class Parser {
+    private static class ParseError extends RuntimeException {}
+
     private final List<Token> tokens;
     private int current = 0;
 
     Parser(List<Token> tokens) {
         this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch(ParseError error) {
+            return null;
+        }
     }
 
     private Expr expression() {
@@ -57,6 +67,33 @@ public class Parser {
         return tokens.get(current - 1);
     }
 
+    private ParseError error(Token token, String message) {
+        Brahma.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() { // Error recovery
+        advance();
+
+        while(!isAtEnd()) {
+            if(previous().type == TokenType.SEMICOLON) return;
+
+            switch(peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+
+            advance();
+        }
+    }
+
     private Expr comparison() {
         Expr expr = term();
 
@@ -67,5 +104,62 @@ public class Parser {
         }
 
         return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while(match(TokenType.MINUS, TokenType.PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while(match(TokenType.SLASH, TokenType.STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if(match(TokenType.BANG, TokenType.MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if(match(TokenType.FALSE)) return new Expr.Literal(false);
+        if(match(TokenType.TRUE)) return new Expr.Literal(true);
+        if(match(TokenType.NIL)) return new Expr.Literal(null);
+
+        if(match(TokenType.NUMBER, TokenType.STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if(match(TokenType.LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expect expression.");
+    }
+
+    private Token consume(TokenType type, String message) {
+        if(check(type)) return advance();
+        throw error(peek(), message);
     }
 }
